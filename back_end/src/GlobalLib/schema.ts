f082b920c0d6949e5120e1c95464d0dcf90ee3dd
Forceps <54647455+Forceps@users.noreply.github.com>
2020-06-23 12:29:36 +0900

    s
    s

--- back_end/src/GlobalLib/schema.ts
+++ back_end/src/GlobalLib/schema.ts
@@ -1,8 +1,9 @@
 import path from "path";
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+import { GraphQLSchema } from "graphql";
 
-const allTypes = fileLoader(
+const allTypes: GraphQLSchema[] = fileLoader(
   path.join(__dirname, "/..", "/GraphQL_API/API/**/*.gql")
 );
 const allResolvers = fileLoader(
