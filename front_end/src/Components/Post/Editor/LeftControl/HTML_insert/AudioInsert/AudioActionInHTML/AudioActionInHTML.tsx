f1fc842cb03561d420a4ef32ed6ca92a1bf214bb
Forceps <54647455+Forceps@users.noreply.github.com>
2020-07-23 22:39:35 +0900

    s
    s

--- front_end/src/Components/Post/Editor/LeftControl/HTML_insert/AudioInsert/AudioActionInHTML/AudioActionInHTML.tsx
+++ front_end/src/Components/Post/Editor/LeftControl/HTML_insert/AudioInsert/AudioActionInHTML/AudioActionInHTML.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import AudioTargetSpecific from "./St1ReusableItems/AudioTargetSpecific";
 
 export default ({ rerenderingPoint }: AudioActionInHTMLProps) => {
-  return <AudioTargetSpecific rerenderingPoint={rerenderingPoint} />;
+  const audioContainers = document.getElementsByClassName("audioPlayer");
+
+  return (
+    <>
+      {(() => {
+        let arr: any[] = [];
+        for (let i = 0; i < audioContainers.length; i++) {
+          arr = arr.concat(
+            <AudioTargetSpecific
+              audioTarget={audioContainers[i]}
+              rerenderingPoint={rerenderingPoint}
+            />
+          );
+        }
+        return arr;
+      })()}
+    </>
+  );
 };
 
 interface AudioActionInHTMLProps {
